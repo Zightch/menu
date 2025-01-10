dbf50820b138fb16e878ef3b35d3167be54a4fd8
Zightch <zightch@163.com>
2025-01-10 14:21:18 +0800

    .

---
+++ src/main/java/top/staticplant/menu/Menu.java
@@ -0,0 +1,10 @@
+package top.staticplant.menu;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Menu implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
